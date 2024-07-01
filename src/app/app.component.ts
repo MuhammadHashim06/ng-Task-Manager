5b459d5a5d51245d5cad923f3e966a54b19d8b1a
MuhammadHashim06 <muhammad.hashimsid@gmail.com>
2024-07-01 16:58:38 +0500

    Make Add Task Form with Styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskformComponent } from "./taskform/taskform.component";
+import { DetailComponent } from "./detail/detail.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, TaskformComponent, DetailComponent,CommonModule]
 })
 export class AppComponent {
+  showform = false
+  toggleform() {
+    this.showform = !this.showform
+  }
   title = 'TaskManager';
 }
